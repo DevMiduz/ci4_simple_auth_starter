02e58daf076dc673c75e203ddf0191cacd9c7695
Dev Miduz <119743168+DevMiduz@users.noreply.github.com>
2023-09-19 15:55:38 +0100

    Added simple.css

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -1,7 +1,12 @@
 <!doctype html>
 <html>
 <head>
-    <title>My Layout</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Simple Auth Starter</title>
+
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
 </head>
 <body>
     <?=$this->renderSection('content')?>
