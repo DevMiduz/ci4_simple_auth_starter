5d887e066ba74e7334220bc6af532e1bf695aac0
Dev Miduz <119743168+DevMiduz@users.noreply.github.com>
2023-09-20 12:25:46 +0100

    Added placeholder controllers and filters

---
+++ app/Controllers/Auth/AccountController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\BaseController;
+
+/**
+ *   Handles functions for Accounts which:
+ *   - already exist.
+ *   - are logged in.
+ *
+ *   View Account Details (profile).
+ *   Change Password.
+ *   Logout.
+ **/
+class AccountController extends BaseController {
+	public function index() {
+		//
+	}
+}
---
+++ app/Controllers/Auth/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\BaseController;
+
+/**
+ *   Handles functions for registering accounts.
+ **/
+class RegisterController extends BaseController {
+	public function index() {
+		//
+	}
+}
