63f580a3e69e7ab9e9e2bee7db6a59c355ebe605
Dev Miduz <119743168+DevMiduz@users.noreply.github.com>
2023-09-25 12:37:01 +0100

    Added password redactions.

--- app/Filters/RequestLogFilter.php
+++ app/Filters/RequestLogFilter.php
@@ -8,6 +8,10 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class RequestLogFilter implements FilterInterface
 {
+    private $password_fields = [
+        'password', 'confirm_password', 'new_password', 'old_password'
+    ];
+
     /**
      * Do whatever processing this filter needs to do.
      * By default it should not return anything during
@@ -25,15 +29,42 @@ class RequestLogFilter implements FilterInterface
      */
     public function before(RequestInterface $request, $arguments = null)
     {
-        /*
-        $audit = [
-            'method' => $request->getMethod(),
-            'path' => $request->(),
-            'ip_address' => $request->getIPAddress(),
-        ];
-        */
+        $request_data = isset($_REQUEST) ? $_REQUEST : [];
+        $this->redact_password_fields($request_data);
+
+        $session = isset($_SESSION) ? $_SESSION : [];
+        $this->redact_password_fields($session);
         
-        log_message('notice', print_r($_SERVER, true));
+        if(isset($session['_ci_old_input'])) {
+            if(isset($session['_ci_old_input']['post'])) {
+                $this->redact_password_fields($session['_ci_old_input']['post']);
+            }
+        }
+        
+
+        $audit = [
+            'REMOTE_ADDR' => $_SERVER['REMOTE_ADDR'],
+            'REQUEST_URI' => $_SERVER['REQUEST_URI'],
+            'REQUEST_METHOD' => $_SERVER['REQUEST_METHOD'],
+            'PATH_INFO' => $_SERVER['PATH_INFO'],
+            'HTTP_USER_AGENT' => $_SERVER['HTTP_USER_AGENT'],
+            'HTTP_ACCEPT' => $_SERVER['HTTP_ACCEPT'],
+            'HTTP_ACCEPT_LANGUAGE' => $_SERVER['HTTP_ACCEPT_LANGUAGE'],
+            'HTTP_ACCEPT_ENCODING' => $_SERVER['HTTP_ACCEPT_ENCODING'],
+            'HTTP_COOKIE' => $_SERVER['HTTP_COOKIE'],
+            'QUERY_STRING' => $_SERVER['QUERY_STRING'],
+            'REQUEST_DATA' => print_r($request_data, true),
+            'SESSION' => print_r($session, true),
+            
+        ];
+
+        /*
+            Should remove passwords from logs.
+        */
+
+
+        
+        log_message('notice', print_r($audit, true));
     }
 
     /**
@@ -50,6 +81,15 @@ class RequestLogFilter implements FilterInterface
      */
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        //
+
+
+    }
+
+    private function redact_password_fields(&$array) {
+        foreach ($this->password_fields as $value) {
+            if(isset($array[$value])) {
+                $array[$value] = 'REDACTED';
+            }
+        }
     }
 }
